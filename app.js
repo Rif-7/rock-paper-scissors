54f899848de97bb356755bb13bf22de5dffb074f
Rif-7 <muhammadrifan974530@gmail.com>
2021-11-13 13:05:21 +0530

    console game finished

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-console.log("test");
+function computerPlay() {
+    return ["rock", "paper", "scissors"][Math.floor(Math.random()*3)];
+}
+
+
+function chooseWinner(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+
+    if (playerChoice === computerChoice) {
+        return "Tie Game";
+    }
+    else if (playerChoice === "rock") {
+        
+        return (computerChoice === "scissors") ? "You Won, Rock beats Scissors" : "You Lose, Paper beats Rock";
+    }
+    else if (playerChoice === "scissors") {
+        return (computerChoice === "rock") ? "You Lose, Rock beats Scissors" : "You Won, Scissors beat Paper";
+    }
+    else if (playerChoice === "paper") {
+        return (computerChoice === "scissors") ? "You Lose, Scissors beat Paper" : "You Won, Paper beats Rock";
+    }
+    else {
+        alert("Invalid Input, Try Again");
+        return chooseWinner(prompt("Rock, Paper, Scissors: "), computerChoice);
+    }
+}
+
+
+function game() {
+    let userPoint = 0;
+    let computerPoint = 0;
+
+    for (let i = 0; i < 3; i++) {
+        let playerChoice = prompt("Rock, Paper or Scissors:");
+        let roundWinner = chooseWinner(playerChoice, computerPlay());
+        alert(roundWinner);
+        console.log(roundWinner);
+        decision = roundWinner.split(",")[0];
+        console.log("      ");
+        if (decision === "You Won") {
+            userPoint++;
+        }
+        else if (decision ===  "You Lose") {
+            computerPoint++;
+        }
+    }
+
+
+    let winner = (userPoint > computerPoint) ? "You won the game" 
+                :(computerPoint > userPoint) ? "You lost the game" 
+                :"Tie Game";
+
+    console.log(`%cYour Total Points: ${userPoint}`, "font-size: 15px;");
+    console.log(`%cCom's Total Points: ${computerPoint}`, "font-size: 15px;");
+    console.log(`%c${winner}`, "font-size: 20px;");
+}
